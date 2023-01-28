64a915a18f2216c035b1a8509436005367f831b9
vrac10 <vrachit106@gmail.com>
2023-01-28 21:13:49 +0530

    check

---
+++ 1.py
@@ -0,0 +1,26 @@
+
+# import datetime
+# import time
+# root = tkinter.Tk()
+# entry = tkinter.Entry(root, width = 29)
+# entry.pack()
+# label = tkinter.Label(root, text= '0')
+# i = 0
+# def i_update():
+#     global i
+#     i += 1
+#     label.config(text = str(i))
+#     if i <= 60:
+#         time.sleep(1)
+#         i_update()
+#     else:
+#         entry.config(state= 'disabled')
+
+# i_update()
+
+print(len('lucky professional national style city music front refuse tennis damage heavily simple '))
+
+
+
+
+
